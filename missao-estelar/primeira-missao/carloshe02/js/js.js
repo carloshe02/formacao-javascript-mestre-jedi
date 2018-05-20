abca4d0d79a52122277501c5befbce7ccd042dca
carloshe02 <carlos.he00@outlook.com>
2018-05-20 11:31:33 -0300

    NOVA-PASTA-ADICIONADA

---
+++ missao-estelar/primeira-missao/carloshe02/js/js.js
@@ -0,0 +1,13 @@
+// Decleração da variável missao //
+var missao = "missão estelar js em execução";
+// ------ Inicio function eventoOnload() ------ //
+function eventoOnload(){
+    console.log("EVENTO ONLOAD DISPARADO");
+}
+// ------ Fim function eventoOnload() ------//
+
+//  ------ function para realizar ação button ------//
+function eventoOnclick(){
+    console.log("misão estelar js realizada com sucesso!");
+}
+// ------------- FIM DO CODÍGO JS------------- //
